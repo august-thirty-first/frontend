6f35ee2193cff99f0f439fc4cc2ed4570f1dace5
tmam444 <cndfuf0159@naver.com>
2023-07-31 14:35:11 +0900

    [refactor]: oauth 주소를 backend 주소로 변경

--- next/src/app/(login)/login/page.tsx
+++ next/src/app/(login)/login/page.tsx
@@ -6,7 +6,7 @@ export default function Login() {
     <div>
       <RedirectBtn
         title="login"
-        redirectUrl="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-fc98ac0844ad77b1a1932b5d97bfd44e98a5a42ed7f89f4140e6c7c6571f2aca&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Foauth&response_type=code"
+        redirectUrl="http://localhost:3000/api/auth/login"
       />
     </div>
   );
