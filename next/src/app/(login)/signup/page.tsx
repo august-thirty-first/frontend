bf85e4d2136848c19b69b414fa5f3c6f50dd2a2f
chung <cndfuf0159@naver.com>
2023-08-03 16:37:45 +0900

    [refactor]: 회원가입 예외 처리 및 컴포넌트 분리
    - 회원가입에서 component 단위로 분리
    - avata 업로드시 용량 설정

--- next/src/app/(login)/signup/page.tsx
+++ next/src/app/(login)/signup/page.tsx
@@ -1,65 +1,74 @@
 'use client';
 
-import Btn from '@/components/btn';
 import WelcomeMessage from '@/components/welcome';
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Nickname from '@/components/(login)/signup/nickname';
+import Avata from '@/components/(login)/signup/avata';
+import commonResponse from '@/lib/interface/commonResponse.interface';
+
+async function signUpAPI(formData: FormData): Promise<commonResponse<void>> {
+  const result: commonResponse<void> = {};
+  const backend_url = 'http://localhost:3000/api';
+  try {
+    const response = await fetch(`${backend_url}/auth/create`, {
+      method: 'POST',
+      credentials: 'include',
+      body: formData,
+    });
+    result.status = response.status;
+    if (!response.ok) result.errorData = await response.json();
+    return result;
+  } catch (error: any) {
+    result.error = error.message;
+    return result;
+  }
+}
 
 export default function SignUp() {
-  const [nickName, setNickName] = useState('');
-  const onHandle = async (e: React.MouseEvent<HTMLElement>) => {
+  const [nickname, setNickname] = useState<string>('');
+  const [isSending, setIsSending] = useState<boolean>(false);
+  const [file, setFile] = useState<File | null>(null);
+  const router = useRouter();
+
+  const onSubmit = async (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
-    try {
-      const response = await fetch(
-        'http://localhost:3000/api/auth/nickname',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            nickname: nickName,
-          }),
-        },
-      );
-      const data = await response.json();
-      if (data.status === true) {
-        alert('중복임');
-      }
-      // console.log(response.json());
-    } catch (error) {
-      console.log('error occure');
-      console.log(error);
+    if (isSending) return;
+    setIsSending(true);
+    setNickname(cur => cur.trim());
+    const formData = new FormData();
+    formData.append('nickname', nickname);
+    if (file) formData.append('avata_path', file);
+    const res: commonResponse<void> = await signUpAPI(formData);
+    if (res.error) {
+      alert(`Error during nickname: ${res.error}`);
+      return;
     }
+    if (res.status === 200) {
+      alert('회원가입 성공');
+      router.replace('/');
+    } else if (res.status === 401) {
+      alert('다시 로그인을 해주세요.');
+      router.push('/login');
+    } else {
+      alert(res.errorData?.message);
+    }
+    setIsSending(false);
   };
+
   return (
     <div>
       <WelcomeMessage />
-      <form
-        action={`http://localhost:3000/api/auth/create`}
-        method="POST"
-        encType="multipart/form-data"
-      >
+      <form>
+        <Nickname nickname={nickname} setNickname={setNickname} />
+        <Avata file={file} setFile={setFile} />
         <div>
-          <label htmlFor="nickname">닉네임</label>
-          <br />
           <input
-            type="text"
-            id="nickname"
-            name="nickname"
-            value={nickName}
-            onChange={event => {
-              setNickName(event.target.value);
-            }}
+            type="submit"
+            onClick={onSubmit}
+            value="제출"
+            disabled={isSending}
           ></input>
-          <Btn title="중복검사" handler={onHandle} />
-        </div>
-        <div>
-          <label htmlFor="avata_path">아바타 사진</label>
-          <br />
-          <input type="file" name="avata_path" id="avata_path"></input>
-        </div>
-        <div>
-          <input type="submit" value="제출"></input>
         </div>
       </form>
     </div>
---
+++ next/src/components/(login)/signup/avata.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { ChangeEvent, Dispatch, SetStateAction } from 'react';
+
+interface avataProps {
+  file: File | null;
+  setFile: Dispatch<SetStateAction<File | null>>;
+}
+
+const Avata = ({ file, setFile }: avataProps) => {
+  const onHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const maxFileSize = 1024 * 1024 * 1;
+    const files = event.target.files;
+    if (files) {
+      const cur_file = files[0];
+      if (cur_file.size >= maxFileSize) {
+        alert('파일 크기는 1MB까지만 가능합니다.');
+        event.target.value = '';
+        setFile(null);
+      } else setFile(files[0]);
+    }
+  };
+  return (
+    <div>
+      <label htmlFor="avata_path">아바타 사진</label>
+      <br />
+      <input
+        type="file"
+        onChange={onHandler}
+        name="avata_path"
+        id="avata_path"
+        accept="image/png, image/jpeg, image/jpg"
+      />
+    </div>
+  );
+};
+
+export default Avata;
