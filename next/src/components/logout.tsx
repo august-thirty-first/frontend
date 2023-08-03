9192f0bec95ffca516e40985a11723308e91001c
chung <cndfuf0159@naver.com>
2023-08-03 16:21:41 +0900

    [bug] : logout시-redirect
    - commonResponse, commonError interface를 사용해서 fetch에 담을 데이터를 설정
    - logout시에 backend 주소로 redirect 되는 logic을 변경
    - client 단에서 get 요청을 보내 cookie를 삭제 처리 해줌

---
+++ next/src/components/logout.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import commonResponse from '@/lib/interface/commonResponse.interface';
+
+async function logoutAPI(): Promise<commonResponse<void>> {
+  const backend_url = 'http://localhost:3000/api';
+  let result: commonResponse<void> = {};
+  try {
+    const response = await fetch(`${backend_url}/auth/logout`, {
+      method: 'GET',
+      credentials: 'include',
+    });
+    result.status = response.status;
+    return result;
+  } catch (error: any) {
+    result.error = error.message;
+    return result;
+  }
+}
+
+export default function Logout() {
+  const [isLoggingOut, setIsLoggingOut] = useState<boolean>(false);
+  const router = useRouter();
+
+  const onClick = async () => {
+    if (isLoggingOut) return;
+    setIsLoggingOut(true);
+    const res = await logoutAPI();
+    if (res.error) {
+      alert(`Error during logout: ${res.error}`);
+      return;
+    }
+    router.replace('/');
+  };
+
+  return <button onClick={onClick}>logout</button>;
+}
