0b468fd9a281f984e2fae07b4a74c2df3728f2db
chung <cndfuf0159@naver.com>
2023-08-04 14:25:22 +0900

    [feat] 로그인 여부에 따른 redirect 처리 추가
    - main page에서 login 되어 있으면 profile page로 이동
    - (home) layout에 token 을 check 하는 코드 추가 => 추후에 검증 처리도 논의

--- next/src/app/(home)/layout.tsx
+++ next/src/app/(home)/layout.tsx
@@ -2,8 +2,13 @@ import childrenProps from '@/interfaces/childrenProps.interface';
 import Navigation from '@/components/navigation';
 import Friends from '@/components/friends';
 import Alarms from '@/components/alarms';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 export default function HomeLayout({ children }: childrenProps) {
+  const token = cookies().get('access_token');
+  if (!token) redirect('/login');
+
   return (
     <div>
       <Navigation />
