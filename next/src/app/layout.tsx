d7efc6cd62c7b60d993852660038ccf19eb61240
yoonkiung <rldnd4429@gmail.com>
2023-08-04 16:00:10 +0900

    [Style]: MVP.css적용 개발할때 기분 좋으라고 임시로 mvp.css적용해 놓았습니다

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://unpkg.com/mvp.css"></link>
+      </head>
       <body>{children}</body>
     </html>
   );
