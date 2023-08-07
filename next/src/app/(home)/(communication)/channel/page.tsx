2a9c04893e2c19a506d917f3fde190bd7b22823d
kimchijinju <hanbimkim@gmail.com>
2023-08-07 14:11:58 +0900

    [Feat]: 채팅 기능 간단히 구현

--- next/src/app/(home)/(communication)/channel/page.tsx
+++ next/src/app/(home)/(communication)/channel/page.tsx
@@ -1,3 +1,44 @@
+'use client';
+import { useContext, useEffect, useState } from "react";
+import { HomeSocketContext } from "@/app/(home)/createHomeSocketContext";
+
 export default function Channel() {
-  return <h1>Channel</h1>;
+  const [value, setValue] = useState('');
+
+  function handleOnSubmit(event: any) {
+    event.preventDefault();
+    socket.emit('message', value);
+    setValue('');
+  }
+
+  useEffect(() => {
+    socket.on('message', (msg) => {
+      setMessages([...messages, msg]);
+      console.log(messages);
+    })
+  }, []);
+
+  const socket = useContext(HomeSocketContext);
+
+  const [messages, setMessages] = useState<string[]>([]);
+
+  return (<div><h1>Channel</h1>
+    <table>
+      <tbody>
+      <tr>
+        <td>
+          {messages.map((message, index) => (
+              <div key={index}>
+                <span>{message}</span>
+              </div>
+          ))}
+        </td>
+      </tr>
+      </tbody>
+    </table>
+    <form onSubmit={handleOnSubmit}>
+      <input value={value} onChange={(event) => setValue(event.target.value)}></input>
+      <button type="submit">send</button>
+    </form>
+  </div>);
 }
