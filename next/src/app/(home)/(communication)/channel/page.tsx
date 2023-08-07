f7cd253d8cc5fcdfeb89333f805686d465006499
kimchijinju <hanbimkim@gmail.com>
2023-08-07 17:30:52 +0900

    [Feat]: 채팅방 입장 기능 구현 - 입장했었던 같은 방에 있던 소켓이었으면 다른 방이어도 채팅이 전송되는 버그가 있습니다

--- next/src/app/(home)/(communication)/channel/page.tsx
+++ next/src/app/(home)/(communication)/channel/page.tsx
@@ -4,20 +4,26 @@ import { HomeSocketContext } from '@/app/(home)/createHomeSocketContext';
 
 export default function Channel() {
   const [value, setValue] = useState('');
+  const [roomName, setRoomName] = useState('');
   const [messages, setMessages] = useState<string[]>([]);
+  const [room, setRoom] = useState('');
   const socket = useContext(HomeSocketContext);
   const messageContainerRef = useRef<HTMLDivElement>(null);
 
   function handleOnSubmit(event: any) {
     event.preventDefault();
     setMessages(prevMessages => [...prevMessages, `You: ${value}`]);
-    socket.emit('message', value);
+    socket.emit('message', { roomName, value });
     setValue('');
   }
 
   useEffect(() => {
-    socket.on('message', msg => {
-      setMessages(prevMessages => [...prevMessages, msg]);
+    socket.on('message', (msg) => {
+      setMessages((prevMessage) => [...prevMessage, msg]);
+      console.log(messages);
+    });
+    socket.on('roomChange', (room) => {
+      setRoom(room);
     });
   }, []);
 
@@ -29,9 +35,23 @@ export default function Channel() {
     }
   }, [messages]);
 
+  function handleOnSubmitRoom(event: any) {
+    event.preventDefault();
+    socket.emit('enterRoom', roomName);
+    setRoomName('');
+  }
+
   return (
     <div>
       <h1>Channel</h1>
+      <h2>join room</h2>
+      <form onSubmit={handleOnSubmitRoom}>
+        <input value={roomName} onChange={(event) => setRoomName(event.target.value)}></input>
+        <button type="submit">send</button>
+      </form>
+      <h1> {room} 입니다.</h1>
+
+      <h2>chat</h2>
       <div
         ref={messageContainerRef}
         style={{ width: '100%', height: '200px', overflow: 'auto' }}
