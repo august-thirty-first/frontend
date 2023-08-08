6b01ee5d299c465537acd38f4f67de9d1fdd1180
flowjiyun <gyp2795@gmail.com>
2023-08-08 22:38:57 +0900

    [fix]: Btn 컴포넌트에 type props 추가

--- next/src/components/btn.tsx
+++ next/src/components/btn.tsx
@@ -2,11 +2,17 @@
 import { MouseEventHandler } from 'react';
 
 export default function Btn({
+  type,
   title,
   handler,
 }: {
+  type?: 'button' | 'submit' | 'reset' | undefined;
   title: React.ReactNode;
   handler?: MouseEventHandler;
 }) {
-  return <button onClick={handler}>{title}</button>;
+  return (
+    <button type={type} onClick={handler}>
+      {title}
+    </button>
+  );
 }
