8b0dd1a51452a3d78f6c7e8b3a0c7019784db227
flowjiyun <gyp2795@gmail.com>
2023-08-08 23:49:51 +0900

    [fix]: Btn 컴포넌트에 disabled 추가

--- next/src/components/btn.tsx
+++ next/src/components/btn.tsx
@@ -5,13 +5,15 @@ export default function Btn({
   type,
   title,
   handler,
+  disabled,
 }: {
   type?: 'button' | 'submit' | 'reset' | undefined;
   title: React.ReactNode;
   handler?: MouseEventHandler;
+  disabled?: boolean;
 }) {
   return (
-    <button type={type} onClick={handler}>
+    <button type={type} onClick={handler} disabled={disabled}>
       {title}
     </button>
   );
