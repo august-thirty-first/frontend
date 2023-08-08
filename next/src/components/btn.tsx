c7e682087c5d6862521c7e4a05b3687a409573b2
yoonkiung <rldnd4429@gmail.com>
2023-08-08 17:13:41 +0900

    [Refactor]: form, btn 컴포넌트 변경 및 추가 - Btn컴포넌트에 type속성 추가해서 form에서도 사용 가능하게 변겅 - form, input, button태그로 이루어진 사용자 입력 받는 폼을 컴포넌트로 생성

--- next/src/components/btn.tsx
+++ next/src/components/btn.tsx
@@ -4,9 +4,15 @@ import { MouseEventHandler } from 'react';
 export default function Btn({
   title,
   handler,
+  type,
 }: {
   title: React.ReactNode;
   handler?: MouseEventHandler;
+  type?: 'button' | 'submit' | 'reset' | undefined;
 }) {
-  return <button onClick={handler}>{title}</button>;
+  return (
+    <button onClick={handler} type={type}>
+      {title}
+    </button>
+  );
 }
