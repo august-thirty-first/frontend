750eb6e7146d6a214b2a3aaad6b1710077536987
kimchijinju <hanbimkim@gmail.com>
2023-08-09 15:36:31 +0900

    [Feat]: 방 생성 기능을 구현하였습니다. - 전송 데이터 room_name, password, status(public, protected, private)

--- next/src/app/(home)/(communication)/channel/layout.tsx
+++ next/src/app/(home)/(communication)/channel/layout.tsx
@@ -1,6 +1,7 @@
 import childrenProps from '@/interfaces/childrenProps.interface';
 import { Metadata } from 'next';
 import EnterRoom from './enterRoom';
+import RedirectBtn from '@/components/redirectBtn';
 
 export const metadata: Metadata = {
   title: 'channel',
@@ -11,8 +12,11 @@ export default function ChannelLayout({ children }: childrenProps) {
   return (
     <div>
       <h1>Channel</h1>
-      <EnterRoom />
-      {children}
+      <div style={{ display: 'flex', gap: '200px' }}>
+        <EnterRoom />
+        <RedirectBtn title={'방 만들기'} redirectUrl={'/channel/createRoom'} />
+        {children}
+      </div>
     </div>
   );
 }
