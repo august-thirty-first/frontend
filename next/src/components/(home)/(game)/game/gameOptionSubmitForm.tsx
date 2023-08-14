54b31c527fd36ed55deb5078cdebc25678053f45
yehan <yehan@student.42seoul.kr>
2023-08-14 16:04:48 +0900

    [Feat]: formData 내용을 소켓으로 바로 보내도록 변경 - formData의 key, value로 객체 -> json으로 만든다

--- next/src/components/(home)/(game)/game/gameOptionSubmitForm.tsx
+++ next/src/components/(home)/(game)/game/gameOptionSubmitForm.tsx
@@ -1,29 +1,13 @@
 'use client';
 
 import Btn from '@/components/btn';
-import DifficultySelection, { Difficulty } from './difficultySelection';
-import TypeSelection, { MapType } from './typeSelection';
+import DifficultySelection from './difficultySelection';
+import TypeSelection from './typeSelection';
 import React, { useContext, useState } from 'react';
-import { useFetch } from '@/lib/useFetch';
 import { GameSocketContext } from '@/app/(home)/(game)/createGameSocketContext';
-
-interface gameOptionResponse {
-  map: MapType;
-  difficulty: Difficulty;
-}
 
 export default function GameOptionSubmitForm() {
   const socket = useContext(GameSocketContext);
-
-  const { statusCodeRef, bodyRef, fetchData, dataRef } =
-    useFetch<gameOptionResponse>({
-      autoFetch: false,
-      url: 'game/option',
-      method: 'POST',
-      // contentType: 'application/json',
-      // contentType: 'multipart/form-data',
-      body: FormData,
-    });
 
   const [isTypeSelected, setIsTypeSelected] = useState<boolean>(false);
   const [isDifficultySelected, setIsDifficultySelected] =
@@ -34,22 +18,13 @@ export default function GameOptionSubmitForm() {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
 
-    // let obj: any = {};
-    // formData.forEach((value, key) => {
-    //   obj[key] = value;
-    // });
-    // const json = JSON.stringify(obj);
-    // bodyRef.current = json;
-
-    bodyRef.current = formData;
-    await fetchData();
-
-    if (statusCodeRef?.current === 201) {
-      if (dataRef?.current) {
-        socket.emit('ready', dataRef.current); //내 소켓 이름으로 보내준다
-        setButtonTitle('Ready!');
-      } else alert('에러');
-    }
+    let obj: any = {};
+    formData.forEach((value, key) => {
+      obj[key] = value;
+    });
+    const stringedJson = JSON.stringify(obj);
+    socket.emit('ready', stringedJson);
+    setButtonTitle('Ready!');
   };
 
   const typeValidateHandler = (isSelected: boolean): void => {
