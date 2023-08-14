a29b88259944ddeb8ed5d5f14fc376bf3741a6ff
kimchijinju <hanbimkim@gmail.com>
2023-08-14 16:23:39 +0900

    [Refactor]: 방 생성 url, 파일명 변경

--- next/src/app/(home)/(communication)/channel/chat/update.tsx
+++ next/src/app/(home)/(communication)/channel/chat/update.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { useSearchParams } from 'next/navigation';
 import Btn from '@/components/btn';
 import { useFetch } from '@/lib/useFetch';
@@ -8,20 +9,42 @@ export default function RoomUpdate() {
   const searchParams = useSearchParams();
   const roomId = searchParams.get('roomId');
   const [showModal, setShowModal] = useState(false);
-  const { isLoading, statusCodeRef, fetchData } = useFetch<void>({
+  const { isLoading, bodyRef, statusCodeRef, fetchData } = useFetch<void>({
     autoFetch: false,
-    method: 'patch',
+    method: 'PATCH',
     url: `chat/${roomId}`,
+    contentType: 'application/json',
   });
 
-  function toggleModal(event: React.MouseEvent<HTMLButtonElement>) {
-    event.preventDefault();
+  function toggleModal() {
+    console.log(showModal);
     setShowModal(!showModal);
   }
+
   async function updateRoomCondition(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
     const formData = new FormData(event.currentTarget);
-    console.log(formData);
+
+    const roomName = formData.get('room_name');
+    const password = formData.get('password');
+    const isPrivate = formData.get('is_private') === 'on';
+
+    let status;
+    if (isPrivate) {
+      status = 'private';
+    } else if (!password) {
+      status = 'public';
+    } else {
+      status = 'protected';
+    }
+
+    bodyRef.current = JSON.stringify({
+      room_name: roomName,
+      password: password,
+      status: status,
+    });
+
     await fetchData();
 
     if (isLoading) return <Btn title={'방 수정중...'} />;
@@ -30,23 +53,27 @@ export default function RoomUpdate() {
     if (statusCodeRef?.current === 200) {
       alert('방 수정 완료');
     } else {
-      alert('방 삭제 실패');
+      alert('방 수정 실패');
     }
   }
 
   return (
     <div>
-      <Btn title={'방 수정'} type={'submit'} handler={toggleModal} />
+      <Btn title={'방 수정'} handler={toggleModal} />
       {showModal && (
-        <form onSubmit={updateRoomCondition}>
-          <input name={'room_name'} placeholder={'방 이름 입력'} />
-          <input name={'password'} placeholder={'방 비밀번호 입력'} />
-          <input
-            name={'room_name'}
-            placeholder={'변경할 상태 (public, private, protected'}
-          />
-          <Btn title={'전송'} type={'submit'} />
-        </form>
+        <Modal closeModal={toggleModal}>
+          <form onSubmit={updateRoomCondition}>
+            <label htmlFor="room_name">방 이름</label>
+            <input name={'room_name'} />
+
+            <label htmlFor="password">비밀번호</label>
+            <input name={'password'} />
+
+            <label htmlFor="is_private">비밀방</label>
+            <input type={'checkbox'} name={'is_private'} />
+            <Btn title={'전송'} type={'submit'} />
+          </form>
+        </Modal>
       )}
     </div>
   );
