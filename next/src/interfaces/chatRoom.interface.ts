97e8da56ff517acdf8bcdb31623de674841cf88e
kimchijinju <hanbimkim@gmail.com>
2023-08-14 16:25:28 +0900

    [Refactor]: response 관련 인터페이스 추가

---
+++ next/src/interfaces/chatRoom.interface.ts
@@ -0,0 +1,14 @@
+enum RoomStatus {
+  public = 'public',
+  protected = 'protected',
+  private = 'private',
+}
+
+export default interface ChatRoom {
+  id: number;
+  room_name: string;
+  status: RoomStatus;
+  password: string;
+  created_at: Date;
+  updated_at: Date;
+}
