3cfb302362a2f06b0c007513ae956ed21b484c93
yehan <yehan@student.42seoul.kr>
2023-08-15 15:27:01 +0900

    [Feat]: main에 반응형으로 canvas 사이즈 결정하도록 함 -의도대로 동작하지 않음

--- next/src/app/(home)/(game)/game/page.tsx
+++ next/src/app/(home)/(game)/game/page.tsx
@@ -1,3 +1,26 @@
-export default function GamePage() {
-  return <h1>Game</h1>;
+'use client'
+
+import { RefObject, useRef } from "react";
+import GameScreen from "./gameScreen";
+import { useClientWidthHeight } from "./useClientWidthHeight";
+
+const style = {
+  width: "1000px",
+  height: "500px",
 }
+export default function GamePage() {
+  const mainRef: RefObject<HTMLElement> = useRef<HTMLElement>(null);
+  const clientRect = useClientWidthHeight(mainRef);
+  const canvasWidth = clientRect.width;
+  const canvasHeight = clientRect.height;
+
+  return (
+    <main style={style} ref={mainRef}>
+      <GameScreen 
+      canvasWidth={canvasWidth}
+      canvasHeight={canvasHeight}
+    />
+    </main>
+  )
+  
+};
