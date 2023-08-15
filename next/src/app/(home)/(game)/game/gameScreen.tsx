21faf028a5d518c925a04f2b656433f971ba43df
yehan <yehan@student.42seoul.kr>
2023-08-15 15:41:09 +0900

    [Feat]: 처음 사용자가 설정해둔 화면을 꽉 채워 검정화면이 나옴 -이후에는 고정됨

--- next/src/app/(home)/(game)/game/gameScreen.tsx
+++ next/src/app/(home)/(game)/game/gameScreen.tsx
@@ -2,34 +2,20 @@
 
 import { RefObject, useEffect, useRef } from "react";
 
-type GameScreenProps = {
-  canvasWidth: number;
-  canvasHeight: number;
-};
-
-const GameScreen
-: React.FC<GameScreenProps> = ({
-  canvasWidth,
-  canvasHeight,
-}) => {
+const GameScreen: React.FC = () => {
     const canvasRef : RefObject<HTMLCanvasElement> = 
   useRef<HTMLCanvasElement>(null);//useRef 훅 호출 (호출하면서 타입이 결정된다?)
   
     useEffect(() => {//canvas 요소가 렌더링(마운트)된 시점을 보장함
       const canvas = canvasRef.current;
       const ctx = canvas?.getContext('2d');//여기에 캔버스 컨텍스트가 할당되는 것이 보장됨
-
-      const setCanvas = () => {
-        if (canvas) {
-          canvas.width = canvasWidth;
-          canvas.height = canvasHeight;
-        }
-      };
-      setCanvas();
-
+      if (canvas) {
+        canvas.width = innerWidth;//처음 윈도우화면을 가져오고, 고정된다.
+        canvas.height = innerHeight;
+      }
       ctx?.fillRect(0, 0, ctx?.canvas.width, ctx?.canvas.height);
 
-    }, [canvasWidth, canvasHeight]);
+    }, []);
   
     return <canvas ref={canvasRef} />;//이 안의 내용을 설정해주고 반환하는 셈
   }
