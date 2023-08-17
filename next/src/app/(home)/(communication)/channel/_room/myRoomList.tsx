c00e9233cf3b450f46a788f1d19fdd6ac43dc6cf
kimchijinju <hanbimkim@gmail.com>
2023-08-17 16:26:06 +0900

    [Refactor]: 파일 이름 변경

--- next/src/app/(home)/(communication)/channel/_room/myRoomList.tsx
+++ next/src/app/(home)/(communication)/channel/_room/myRoomList.tsx
@@ -1,6 +1,6 @@
 import RoomList from '@/app/(home)/(communication)/channel/_room/roomList';
 
-export default function MyChatRoomList() {
+export default function MyRoomList() {
   return (
     <div>
       <h2> 내 채팅방 목록 </h2>
