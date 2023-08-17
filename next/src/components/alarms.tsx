75e9c863132a1e8c95f5471921ee41e4339a1dc0
chung <cndfuf0159@naver.com>
2023-08-17 09:53:50 +0900

    [refactor] JSX 조건문 변경
    - data && 조건문을 불필요하게 두번 사용한것으로 보여 리팩토링

--- next/src/components/alarms.tsx
+++ next/src/components/alarms.tsx
@@ -27,41 +27,36 @@ export default function Alarms() {
   return (
     <div className="max-w-2xl border-4">
       알람 목록
-      {data && (
-        <ul
-          role="list"
-          className="divide-y divide-gray-200 dark:divide-gray-700"
-        >
-          {data &&
-            data.map(friend => (
-              <li className="py-3 sm:py-4" key={friend.nickname}>
-                <div className="flex items-center space-x-4">
-                  <div className="flex-shrink-0">
-                    <Image
-                      width="200"
-                      height="200"
-                      className="h-8 w-8 rounded-full"
-                      src={
-                        friend.avata_path
-                          ? `http://nestjs:3000/${friend.avata_path}`
-                          : defaultImg
-                      }
-                      alt="avatar"
-                    />
-                  </div>
-                  <div className="flex-1 min-w-0">
-                    <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                      {friend.nickname}
-                    </p>
-                  </div>
-                  <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                    <FriendBtn id={friend.id} status={friend.status} />
-                  </div>
+      <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
+        {data &&
+          data.map(friend => (
+            <li className="py-3 sm:py-4" key={friend.nickname}>
+              <div className="flex items-center space-x-4">
+                <div className="flex-shrink-0">
+                  <Image
+                    width="200"
+                    height="200"
+                    className="h-8 w-8 rounded-full"
+                    src={
+                      friend.avata_path
+                        ? `http://nestjs:3000/${friend.avata_path}`
+                        : defaultImg
+                    }
+                    alt="avatar"
+                  />
                 </div>
-              </li>
-            ))}
-        </ul>
-      )}
+                <div className="flex-1 min-w-0">
+                  <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                    {friend.nickname}
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  <FriendBtn id={friend.id} status={friend.status} />
+                </div>
+              </div>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
--- next/src/components/friends.tsx
+++ next/src/components/friends.tsx
@@ -32,45 +32,40 @@ export default function Friends() {
   return (
     <div className="max-w-2xl border-4">
       친구 목록
-      {data && (
-        <ul
-          role="list"
-          className="divide-y divide-gray-200 dark:divide-gray-700"
-        >
-          {data &&
-            data.map(friend => (
-              <li className="py-3 sm:py-4" key={friend.nickname}>
-                <div className="flex items-center space-x-4">
-                  <div className="flex-shrink-0">
-                    <Image
-                      width="200"
-                      height="200"
-                      className="h-8 w-8 rounded-full"
-                      src={
-                        friend.avata_path
-                          ? `http://nestjs:3000/${friend.avata_path}`
-                          : defaultImg
-                      }
-                      alt="avatar"
-                    />
-                  </div>
-                  <div className="flex-1 min-w-0">
-                    <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                      {friend.nickname}
-                    </p>
-                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                      {friend.status}
-                    </p>
-                  </div>
-                  <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                    <Btn title="DM" />
-                    <Btn title="PLAY" />
-                  </div>
+      <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
+        {data &&
+          data.map(friend => (
+            <li className="py-3 sm:py-4" key={friend.nickname}>
+              <div className="flex items-center space-x-4">
+                <div className="flex-shrink-0">
+                  <Image
+                    width="200"
+                    height="200"
+                    className="h-8 w-8 rounded-full"
+                    src={
+                      friend.avata_path
+                        ? `http://nestjs:3000/${friend.avata_path}`
+                        : defaultImg
+                    }
+                    alt="avatar"
+                  />
                 </div>
-              </li>
-            ))}
-        </ul>
-      )}
+                <div className="flex-1 min-w-0">
+                  <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                    {friend.nickname}
+                  </p>
+                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                    {friend.status}
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  <Btn title="DM" />
+                  <Btn title="PLAY" />
+                </div>
+              </div>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
