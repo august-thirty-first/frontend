8114e5c40d83f8a26d4853dbcc2a9819822c6da0
chung <cndfuf0159@naver.com>
2023-08-18 21:14:20 +0900

    [refactor]: logout, createHomeScoketContext file
    - fetch 함수를 useFetch로 변경
    - createHomeScoketContext에 event listener 설정
      => 이렇게 해야지 game page에서도 event 처리가 가능

--- next/src/app/(home)/createHomeSocketContext.tsx
+++ next/src/app/(home)/createHomeSocketContext.tsx
@@ -1,6 +1,8 @@
 'use client';
 import io from 'socket.io-client';
 import { createContext, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useFetch } from '@/lib/useFetch';
 
 const isClient = typeof window !== 'undefined';
 const homeSocket = isClient
@@ -11,6 +13,13 @@ const homeSocket = isClient
 
 export const HomeSocketContext = createContext(homeSocket);
 const HomeSocketProvider = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+  const { fetchData } = useFetch<void>({
+    autoFetch: false,
+    url: 'auth/logout',
+    method: 'GET',
+  });
+
   useEffect(() => {
     homeSocket.on('connect', () => {
       console.log('hello ');
@@ -18,7 +27,17 @@ const HomeSocketProvider = ({ children }: { children: React.ReactNode }) => {
     homeSocket.on('connection', msg => {
       console.log(msg);
     });
-  }, []);
+    homeSocket.on('multipleConnect', () => {
+      alert('다중 로그인 상태.');
+      router.push('/login');
+    });
+    homeSocket.on('expired', async () => {
+      alert('서버와의 연결이 끊어졌습니다. 다시 로그인해주세요.');
+      await fetchData();
+      if (homeSocket.connected) homeSocket.disconnect();
+      router.replace('/login');
+    });
+  }, [router, fetchData]);
   return (
     <HomeSocketContext.Provider value={homeSocket}>
       {children}
