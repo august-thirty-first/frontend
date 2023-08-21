83c41e55bd2d04efa750d12b3833649a98694273
chung <cndfuf0159@naver.com>
2023-08-21 12:54:49 +0900

    [refactor] HomeSocketContext 수정
    - Destructor에 homeSocket event listener 삭제 처리
    - alert -> alertModal로 수정

--- next/src/app/(home)/createHomeSocketContext.tsx
+++ next/src/app/(home)/createHomeSocketContext.tsx
@@ -3,6 +3,7 @@ import io from 'socket.io-client';
 import { createContext, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useFetch } from '@/lib/useFetch';
+import { useShowModal } from '../ShowModalContext';
 
 const isClient = typeof window !== 'undefined';
 const homeSocket = isClient
@@ -14,6 +15,7 @@ const homeSocket = isClient
 export const HomeSocketContext = createContext(homeSocket);
 const HomeSocketProvider = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
+  const alertModal = useShowModal();
   const { fetchData } = useFetch<void>({
     autoFetch: false,
     url: 'auth/logout',
@@ -28,16 +30,23 @@ const HomeSocketProvider = ({ children }: { children: React.ReactNode }) => {
       console.log(msg);
     });
     homeSocket.on('multipleConnect', () => {
-      alert('다중 로그인 상태.');
+      alertModal('다중 로그인 상태.');
       router.push('/login');
     });
     homeSocket.on('expired', async () => {
-      alert('서버와의 연결이 끊어졌습니다. 다시 로그인해주세요.');
+      alertModal('서버와의 연결이 끊어졌습니다. 다시 로그인해주세요.');
       await fetchData();
       if (homeSocket.connected) homeSocket.disconnect();
       router.replace('/login');
     });
-  }, [router, fetchData]);
+    return () => {
+      homeSocket.off('connect');
+      homeSocket.off('connection');
+      homeSocket.off('multipleConnect');
+      homeSocket.off('expired');
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <HomeSocketContext.Provider value={homeSocket}>
       {children}
