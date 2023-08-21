3c92112a2949080548157a220101d8af29d55016
kimchijinju <hanbimkim@gmail.com>
2023-08-21 15:21:08 +0900

    [Fix]: 채팅 방 나가기 기능 구현 - 추후 소켓에 관한 기능 구현이 필요합니다.

---
+++ next/src/app/(home)/(communication)/channel/chat/[id]/leave.tsx
@@ -0,0 +1,38 @@
+import { useParams, useRouter } from 'next/navigation';
+import { useFetch } from '@/lib/useFetch';
+import Btn from '@/components/btn';
+import { useContext } from 'react';
+import { HomeSocketContext } from '@/app/(home)/createHomeSocketContext';
+import { useMyParticipantInfo } from '@/app/(home)/(communication)/channel/MyParticipantInfoContext';
+import { ParticipantAuthority } from '@/interfaces/chatParticipant.interface';
+
+export default function RoomLeave() {
+  const params = useParams();
+  const roomId = params.id;
+  const router = useRouter();
+  const socket = useContext(HomeSocketContext);
+  const [myParticipantInfo] = useMyParticipantInfo();
+
+  const { isLoading, statusCodeRef, fetchData } = useFetch<void>({
+    autoFetch: false,
+    method: 'DELETE',
+    url: `chat/participant/leave/${roomId}`,
+  });
+
+  // Todo: 기존에 방에 참여하고 있었던 사용자에 대한 처리 필요
+  // socket.on('leaveRoom', () => {
+  //   leaveRoom();
+  // });
+
+  async function leaveRoom() {
+    await fetchData();
+
+    // if i am boss? send leaveRoom event
+    // if (myParticipantInfo?.authority === ParticipantAuthority.BOSS) {
+    //   socket.emit('leaveRoom', JSON.stringify({ roomId: roomId }));
+    // }
+    router.push(`/channel/`);
+  }
+
+  return <Btn title={'방 나가기'} type={'button'} handler={leaveRoom} />;
+}
