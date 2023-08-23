59660fa70006ecfc9f6a35665bdd1afe269831fb
yehan <yehan@student.42seoul.kr>
2023-08-23 17:08:32 +0900

    [Feat]: 새로고침, 뒤로가기 리슨 & 소켓 끊겼을 때 모달

--- next/src/app/(home)/(game)/createGameSocketContext.tsx
+++ next/src/app/(home)/(game)/createGameSocketContext.tsx
@@ -1,6 +1,7 @@
 'use client';
 import io from 'socket.io-client';
-import { createContext } from 'react';
+import { createContext, useEffect } from 'react';
+import { useModal } from './modalProvider';
 
 const isClient = typeof window !== 'undefined';
 const gameSocket = isClient
@@ -11,6 +12,28 @@ const gameSocket = isClient
 
 export const GameSocketContext = createContext(gameSocket);
 const GameSocketProvider = ({ children }: { children: React.ReactNode }) => {
+  const { openModal } = useModal();
+  useEffect(() => {
+    const disconnectListener = () => {
+      openModal('연결이 끊겼습니다..'); //새로고침, 뒤로가기를 포함하여 소켓이 끊기는 모든 상황을 처리하기 위해, 여기서 띄운다
+    };
+    gameSocket.on('disconnect', disconnectListener);
+
+    //새로고침
+    window.addEventListener('beforeunload', event => {
+      gameSocket.disconnect();
+    });
+
+    //뒤로가기
+    window.addEventListener('popstate', event => {
+      gameSocket.disconnect();
+    });
+
+    return () => {
+      gameSocket.off('disconnect', disconnectListener);
+    };
+  }, []);
+
   return (
     <GameSocketContext.Provider value={gameSocket}>
       {children}
