f9aec080bd0ad5d3d1345a3ef9ea1f5763f07e10
yehan <yehan@student.42seoul.kr>
2023-08-23 13:53:50 +0900

    [Feat]: gameOverInOptionPage 이벤트 받으면 모달창 띄움

--- next/src/components/(home)/(game)/game/gameOptionSubmitForm.tsx
+++ next/src/components/(home)/(game)/game/gameOptionSubmitForm.tsx
@@ -6,10 +6,12 @@ import TypeSelection from './typeSelection';
 import React, { useContext, useEffect, useState } from 'react';
 import { GameSocketContext } from '@/app/(home)/(game)/createGameSocketContext';
 import { useRouter } from 'next/navigation';
+import { useModal } from '@/app/(home)/(game)/modalProvider';
 
 export default function GameOptionSubmitForm() {
   const socket = useContext(GameSocketContext);
   const router = useRouter();
+  const { openModal } = useModal();
 
   const [isTypeSelected, setIsTypeSelected] = useState<boolean>(false);
   const [isDifficultySelected, setIsDifficultySelected] =
@@ -42,6 +44,12 @@ export default function GameOptionSubmitForm() {
       router.push('/game');
     });
   }, [socket, router]);
+
+  //중간에 상대방 소켓이 끊어졌을 때 모달창을 띄운다
+  const gameOverInOptionPageListener = () => {
+    openModal('상대방이 떠났습니다..');
+  };
+  socket.on('gameOverInOptionPage', gameOverInOptionPageListener);
 
   return (
     <div>
