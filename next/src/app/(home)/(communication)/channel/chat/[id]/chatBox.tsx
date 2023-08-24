9a4e7238f7978cfc2eddad7fbcbfe901f84044e1
chung <cndfuf0159@naver.com>
2023-08-24 17:23:32 +0900

    [feat] DM 기능 개발
    - useEffect에서 directMessage event listener 설정
    - handleOnSubmit에서 DM 정규식을 확인해서 일반 메세지, 귓속말 구분
    - sendDirectMessage 함수에서 해당 nickname의 유저 id 값 가져오기

--- next/src/app/(home)/(communication)/channel/chat/[id]/chatBox.tsx
+++ next/src/app/(home)/(communication)/channel/chat/[id]/chatBox.tsx
@@ -3,6 +3,8 @@ import SubmitForm from '@/components/submitForm';
 import { useParams } from 'next/navigation';
 import { useContext, useEffect, useRef, useState } from 'react';
 import { HomeSocketContext } from '@/app/(home)/createHomeSocketContext';
+import { useFetch } from '@/lib/useFetch';
+import { searchProfileResponse } from '../../../profile/searchBar';
 
 export default function ChatBox() {
   const params = useParams();
@@ -11,16 +13,28 @@ export default function ChatBox() {
   const messageContainerRef = useRef<HTMLDivElement>(null);
   const [messages, setMessages] = useState<string[]>([]);
   const [inputMessage, setInputMessage] = useState<string>('');
+  const { fetchData, dataRef, urlRef } = useFetch<searchProfileResponse>({
+    autoFetch: false,
+    url: `profile/user?nickname=$nickname`,
+    method: 'GET',
+  });
 
   useEffect(() => {
-    setMessages(prevMessage => []);
+    setMessages([]);
   }, [roomId]);
 
   useEffect(() => {
-    socket.on('message', msg => {
+    const messageListener = (msg: string) => {
       setMessages(prevMessage => [...prevMessage, msg]);
       console.log(`'message' : ${msg}`);
-    });
+    };
+    socket.on('message', messageListener);
+    socket.on('directMessage', messageListener);
+    return () => {
+      socket.off('message');
+      socket.off('directMessage', messageListener);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
@@ -30,13 +44,42 @@ export default function ChatBox() {
     }
   }, [messages]);
 
+  async function sendDirectMessage(nickname: string, inputMessage: string) {
+    urlRef.current = `profile/user?nickname=${nickname}`;
+    await fetchData();
+    if (dataRef?.current) {
+      const directMsg = {
+        targetUserId: dataRef.current.id,
+        inputMessage: inputMessage,
+      };
+      socket.emit('directMessage', JSON.stringify(directMsg));
+    } else {
+      setMessages(prevMessages => [
+        ...prevMessages,
+        `찾을 수 없는 사용자입니다.`,
+      ]);
+    }
+  }
+
   function handleOnSubmit(event: any) {
     event.preventDefault();
-    setMessages(prevMessages => [...prevMessages, `You: ${inputMessage}`]);
-    socket.emit(
-      'message',
-      JSON.stringify({ roomId: roomId, inputMessage: inputMessage }),
-    );
+
+    if (inputMessage.startsWith('/')) {
+      const dmRegex = /^\/w\s+(\S+)\s+(.*)$/;
+      const regexMatch = inputMessage.match(dmRegex);
+      if (regexMatch) sendDirectMessage(regexMatch[1], regexMatch[2]);
+      else
+        setMessages(prevMessages => [
+          ...prevMessages,
+          `귓속말 사용법 : /w {user_id} {message}`,
+        ]);
+    } else {
+      setMessages(prevMessages => [...prevMessages, `You: ${inputMessage}`]);
+      socket.emit(
+        'message',
+        JSON.stringify({ roomId: roomId, inputMessage: inputMessage }),
+      );
+    }
     setInputMessage('');
   }
 
