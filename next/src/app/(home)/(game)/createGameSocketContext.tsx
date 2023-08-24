4ede4c0abcabd05b71a71d91b620d0bb3cae268c
yehan <yehan@student.42seoul.kr>
2023-08-24 10:14:58 +0900

    (임시저장): 뒤로가기, 새로고침 작업 중 -validate를 merge 후 작업 예정

--- next/src/app/(home)/(game)/createGameSocketContext.tsx
+++ next/src/app/(home)/(game)/createGameSocketContext.tsx
@@ -1,7 +1,7 @@
 'use client';
 import io from 'socket.io-client';
 import { createContext, useEffect } from 'react';
-import { useModal } from './modalProvider';
+import { useRouter } from 'next/navigation';
 
 const isClient = typeof window !== 'undefined';
 const gameSocket = isClient
@@ -12,22 +12,21 @@ const gameSocket = isClient
 
 export const GameSocketContext = createContext(gameSocket);
 const GameSocketProvider = ({ children }: { children: React.ReactNode }) => {
-  const { openModal } = useModal();
+  const router = useRouter();
   useEffect(() => {
     const disconnectListener = () => {
-      openModal('연결이 끊겼습니다..'); //새로고침, 뒤로가기를 포함하여 소켓이 끊기는 모든 상황을 처리하기 위해, 여기서 띄운다
+      // openModal('연결이 끊겼습니다..'); //새로고침, 뒤로가기하면 이 모달창이 안 보인다
+      router.push('/profile'); // 새로고침 시 무시됨. validate를 통해 소켓 연결 안 시켜주는 작업 필요
     };
     gameSocket.on('disconnect', disconnectListener);
 
     //새로고침
     window.addEventListener('beforeunload', event => {
-      gameSocket.disconnect();
+      event.returnValue = ''; //한 번 더 물어본다. unload를 선택하면 소켓이 끊어지고, 다시 연결되기 전에 바로 홈으로 돌아간다.
     });
 
-    //뒤로가기
-    window.addEventListener('popstate', event => {
-      gameSocket.disconnect();
-    });
+    //TODO:뒤로가기
+    window.addEventListener('popstate', event => {});
 
     return () => {
       gameSocket.off('disconnect', disconnectListener);
