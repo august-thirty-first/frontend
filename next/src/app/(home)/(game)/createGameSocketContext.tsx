400812e23c4758819bdb8a07e62388401b131902
yehan <yehan@student.42seoul.kr>
2023-08-25 14:53:27 +0900

    [Tested]: 일부 동작 새로고침 시 ladder, game 뒤로가기 시 ladder 제외하고 동작하는 상황

--- next/src/app/(home)/(game)/createGameSocketContext.tsx
+++ next/src/app/(home)/(game)/createGameSocketContext.tsx
@@ -10,29 +10,56 @@ const gameSocket = isClient
     })
   : io();
 
+/*
+1. validate
+    - option 0
+    - game: 미구현
+2. 동일 유저
+    - ladder 0
+    - option 0
+    - game 0
+3. 새로고침
+    - ladder ***
+    문제:
+      끊어지고 재연결되면서 다시 큐에 들어가지만, 화면은 profile로 라우팅 되는 상황
+    해결:
+      재연결되는 것도 끊어주거나 (다른 곳에서는 validate로 끊어줌)
+      profile로 라우팅되지 않게 해야 함
+    - option 0
+    한쪽이 끊어지면 서버에서 감지해서 다른 쪽도 알려주고
+    끊긴 자신은 홈으로 간다
+    - game ***
+      : renderInfo 읽을 수 없음 에러 발생 (재연결하면서)
+4. 뒤로가기
+    - ladder ***
+      : 소켓이 안 끊어짐
+    - option 0
+    - game 0
+*/
+
 export const GameSocketContext = createContext(gameSocket);
 const GameSocketProvider = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
   useEffect(() => {
     const disconnectListener = () => {
-      // openModal('연결이 끊겼습니다..'); //새로고침, 뒤로가기하면 이 모달창이 안 보인다
-      router.push('/profile'); // 새로고침 시 무시됨. validate를 통해 소켓 연결 안 시켜주는 작업 필요
+      router.push('/profile');
     };
     gameSocket.on('disconnect', disconnectListener);
 
     //새로고침
-    const beforeunloadListener = (event: any) => {
-      event.returnValue = ''; //한 번 더 물어본다. unload를 선택하면 소켓이 끊어지고, 다시 연결되기 전에 바로 홈으로 돌아간다.
-    };
-    window.addEventListener('beforeunload', beforeunloadListener);
+    // const beforeunloadListener = () => {};
+    // window.addEventListener('beforeunload', beforeunloadListener);
 
     //TODO:뒤로가기
-    window.addEventListener('popstate', event => {});
+    const popstateListener = () => {
+      gameSocket.disconnect();
+    };
+    window.addEventListener('popstate', popstateListener);
 
     return () => {
       gameSocket.off('disconnect', disconnectListener);
-      window.removeEventListener('beforeunload', beforeunloadListener);
-      // window.removeEventListener('popstate', popstateListener);
+      // window.removeEventListener('beforeunload', beforeunloadListener);
+      window.removeEventListener('popstate', popstateListener);
     };
   }, []);
 
