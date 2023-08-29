6ae23d0f1033e4417a09a81045c85baef93bc09e
yoonkiung <rldnd4429@gmail.com>
2023-08-29 15:14:50 +0900

    [Feat]: 권한 업데이트때 소켓 이벤트를 받음 - 인터페이스 생성 - 관리자일때랑 아닐때 버튼 title, handle함수 구분 - 권한 바뀐 사용자는 즉시 관리자 전용 버튼 활성화 가능

--- next/src/app/(home)/(communication)/channel/chat/[id]/_participant/chatParticipant.tsx
+++ next/src/app/(home)/(communication)/channel/chat/[id]/_participant/chatParticipant.tsx
@@ -1,4 +1,6 @@
-import ChatParticipant from '@/interfaces/chatParticipant.interface';
+import ChatParticipant, {
+  ChatParticipantWithBlackList,
+} from '@/interfaces/chatParticipant.interface';
 import Btn from '@/components/btn';
 import { useState } from 'react';
 import ParticipantDetails from '@/app/(home)/(communication)/channel/chat/[id]/_participant/participantDetails';
@@ -7,31 +9,34 @@ export default function ChatParticipantList({
   participants,
   roomId,
 }: {
-  participants: ChatParticipant[];
+  participants: ChatParticipantWithBlackList[];
   roomId: number;
 }) {
-  const [showDetails, setShowDetails] = useState<ChatParticipant | null>(null);
+  const [showDetails, setShowDetails] =
+    useState<ChatParticipantWithBlackList | null>(null);
 
   return (
     <div>
       <label>참여자 목록</label>
       {participants &&
-        participants.map((participant: ChatParticipant, index: number) => (
-          <div key={index}>
-            <Btn
-              title={`${participant.user.nickname}`}
-              type={'button'}
-              handler={() => {
-                setShowDetails(
-                  showDetails === participant ? null : participant,
-                );
-              }}
-            />
-            {showDetails === participant && (
-              <ParticipantDetails roomId={roomId} participant={participant} />
-            )}
-          </div>
-        ))}
+        participants.map(
+          (participant: ChatParticipantWithBlackList, index: number) => (
+            <div key={index}>
+              <Btn
+                title={`${participant.user.nickname}`}
+                type={'button'}
+                handler={() => {
+                  setShowDetails(
+                    showDetails === participant ? null : participant,
+                  );
+                }}
+              />
+              {showDetails === participant && (
+                <ParticipantDetails roomId={roomId} participant={participant} />
+              )}
+            </div>
+          ),
+        )}
     </div>
   );
 }
